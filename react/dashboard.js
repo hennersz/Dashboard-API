db96bbc5e9b9327e96f81a3eb1fa91bcf0cd4cbd
Henry Mortimer <henry@morti.plus.com>
2018-09-12 19:44:09 +0100

    add time widget

--- react/dashboard.js
+++ react/dashboard.js
@@ -11,14 +11,33 @@ class DepartureBoard extends React.Component {
         <div className='centered'>
           <h4>{stationNames[this.props.dep]} → {stationNames[this.props.dest]}</h4>
         </div>
+        <div className="flexRow">
+          <div className="flex1-3">
+            <h4>Departure Time</h4>
+          </div>
+          <div className="flex1-3">
+            <h4>Platform</h4>
+          </div>
+          <div className="flex1-3">
+            <h4>Arrival Time</h4>
+          </div>
+        </div>
         <div style={{height: '70%', overflow: 'scroll'}}>
           {this.props.services.map((service, i) => (
             <div key={i}>
               <div className='service flexRow'>
-                <div className='padded flex1-3'>{service.delay === 'On time' ? service.departureTime : service.delay}</div>
+                <div className='padded flex1-3'>
+                  { service.departureTime }
+                  <br/>
+                  {service.delay === 'On time' ? null : `(${service.delay})`}
+                </div>
                 <div className='padded flex1-3'>{service.platform ? service.platform : 'Platform Unavailable'}</div>
 
-                <div className='padded flex1-3'>{service.arrivalDelay === 'On time' ? service.arrivalTime : service.arrivalDelay}</div>
+                <div className='padded flex1-3'>
+                  { service.arrivalTime }
+                  <br/>
+                  {service.arrivalDelay === 'On time' ? null : `(${service.arrivalDelay})`}
+                </div>
               </div>
               {i !== this.props.services.length - 1 ? <hr /> : null}
             </div>
@@ -34,6 +53,7 @@ class DepartureBoardContainer extends React.Component {
     super(props);
     this.state = {
       journeys: window.serverData
+
     };
   }
 
@@ -53,8 +73,44 @@ class DepartureBoardContainer extends React.Component {
             />
           </div>
         ))}
+        <div className="flex1-2">
+          <TimeWidget/>
+        </div>
       </div>
     );
+  }
+}
+
+class TimeWidget extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeNow: moment().format('LTS'),
+      dateNow: moment().format('dddd Do MMMM YYYY')
+    };
+  }
+
+  updateDateTime(){
+    this.setState({
+      timeNow: moment().format('LTS'),
+      dateNow: moment().format('dddd Do MMMM YYYY')
+    });
+  }
+
+  componentDidMount(){
+    this.intervalId = setInterval(this.updateDateTime.bind(this), 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return <div className="board">
+      {this.state.timeNow}
+      <br/>
+      {this.state.dateNow}
+    </div>;
   }
 }
 
